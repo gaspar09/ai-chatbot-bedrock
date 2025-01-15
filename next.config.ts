7613873f45b6c2a21ad7e51bcff72857dbaf98ba
Gaspar Garcia <gaspar@vercel.com>
2025-01-14 18:07:51 -0800

    add common env vars to next config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,13 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
     ppr: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        hostname: 'avatar.vercel.sh',
-      },
-    ],
+  env: {
+    OTEL_LOG_LEVEL: "all",
+    OTEL_TRACES_SAMPLER: "parentbased_traceidratio",
+    OTEL_TRACES_SAMPLER_ARG: "1",
   },
 };
 
